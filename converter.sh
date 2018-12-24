95f943471ac206a2768ab54197eabf848bad5931
Ethan Steininger <esteininger21@gmail.com>
2018-12-24 13:13:06 -0500

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -4,7 +4,16 @@ set -eux
 
 FILE=$(basename $1)
 
+echo "downloading file...."
+
 wget -O $FILE $1
 
-echo "Converting to flac for cloud services"
-ffmpeg -i $FILE -ar 16000 $2
+echo "spliting into parts"
+ffmpeg -i $1 -f segment -segment_time $2 -c copy files/mp3s/out%03d.mp3
+
+COUNTER=1
+for file in files/mp3s/*
+do
+  yes | ffmpeg -i $file -ac 1 -ar 16000 files/flacs/$COUNTER.flac
+  COUNTER=$[$COUNTER +1]
+done
