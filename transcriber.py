b28bc61c63e7f5aebc5162e04948a309f3cc002f
Ethan Steininger <esteininger21@gmail.com>
2018-12-24 13:14:02 -0500

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -13,6 +13,7 @@ def transcribe(file_name):
 
     # Loads the audio into memory
     with io.open(file_name, 'rb') as audio_file:
+        print ('loading in mem')
         content = audio_file.read()
         audio = types.RecognitionAudio(content=content)
         os.remove(file_name)
@@ -23,10 +24,12 @@ def transcribe(file_name):
         language_code='en-US')
 
     # Detects speech in the audio file
+    print ('running')
     response = client.recognize(config, audio)
 
     transcript = []
     for result in response.results:
+        print ('output')
         transcript.append(result.alternatives[0].transcript)
 
     return transcript
